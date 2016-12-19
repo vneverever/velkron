e05cfaba753c30d14ac79811bc0fa8fdafd78ab4
Anton Vetter <vneverever@gmail.com>
2016-12-19 15:30:01 +0100

    test

--- config/app.php
+++ config/app.php
@@ -185,8 +185,8 @@ return [
          * See: https://github.com/acacha/llum
          */
 
-        Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
-        Barryvdh\Debugbar\ServiceProvider::class,
+        //Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
+        //Barryvdh\Debugbar\ServiceProvider::class,
         #llum_providers
 
     ],
