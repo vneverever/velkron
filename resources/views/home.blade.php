8cb9274eea05f315a267f3cc8f62701703d8933b
Anton Vetter <vneverever@gmail.com>
2016-12-19 18:23:58 +0100

    test

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -62,22 +62,24 @@
                 </div>
             </div>
         </div>
-        <div class="box box-primary">
-            <div class="box-header with-border">
-                <h3 class="box-title">Area Chart</h3>
+        <div class="col-lg-12 col-md-6">
+            <div class="box box-primary">
+                <div class="box-header with-border">
+                    <h3 class="box-title">Area Chart</h3>
 
-                <div class="box-tools pull-right">
-                    <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
-                    </button>
-                    <button type="button" class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i></button>
+                    <div class="box-tools pull-right">
+                        <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
+                        </button>
+                        <button type="button" class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i></button>
+                    </div>
                 </div>
-            </div>
-            <div class="box-body">
-                <div class="chart">
-                    <canvas id="areaChart" style="height: 250px; width: 787px;" height="250" width="787"></canvas>
+                <div class="box-body">
+                    <div class="chart">
+                        <canvas id="areaChart" style="height: 250px; width: 787px;" height="250" width="787"></canvas>
+                    </div>
                 </div>
+                <!-- /.box-body -->
             </div>
-            <!-- /.box-body -->
         </div>
 	</div>
 @endsection
