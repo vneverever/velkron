82e275f5c883a8e367fa579fb0981279c114106c
Anton Vetter <vneverever@gmail.com>
2016-12-19 18:15:32 +0100

    test

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,31 +10,56 @@
 
 @section('main-content')
 	<div class="container-fluid spark-screen">
-		<div class="row">
-			<div class="col-md-8 col-md-offset-2">
-				<div class="panel panel-default">
-					<div class="panel-heading">Home</div>
-
-					<div class="panel-body">
-						{{ trans('adminlte_lang::message.logged') }}
-					</div>
-				</div>
-			</div>
-		</div>
         <div class="col-lg-3 col-xs-6">
             <!-- small box -->
             <div class="small-box bg-green">
                 <div class="inner">
-                    <h3>55<sup style="font-size: 20px">$</sup></h3>
+                    <h3>55.73<sup style="font-size: 20px">$</sup></h3>
 
-                    <p>Bounce Rate</p>
+                    <p>Today</p>
                 </div>
                 <div class="icon">
-                    <i class="ion ion-stats-bars"></i>
+                    <i class="fa fa-dollar"></i>
                 </div>
-                <a href="#" class="small-box-footer">
-                    More info <i class="fa fa-arrow-circle-right"></i>
-                </a>
+            </div>
+        </div>
+        <div class="col-lg-3 col-xs-6">
+            <!-- small box -->
+            <div class="small-box bg-green">
+                <div class="inner">
+                    <h3>77.03<sup style="font-size: 20px">$</sup></h3>
+
+                    <p>Yesterday</p>
+                </div>
+                <div class="icon">
+                    <i class="fa fa-dollar"></i>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-3 col-xs-6">
+            <!-- small box -->
+            <div class="small-box bg-green">
+                <div class="inner">
+                    <h3>323.58<sup style="font-size: 20px">$</sup></h3>
+
+                    <p>This Month</p>
+                </div>
+                <div class="icon">
+                    <i class="fa fa-dollar"></i>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-3 col-xs-6">
+            <!-- small box -->
+            <div class="small-box bg-green">
+                <div class="inner">
+                    <h3>5329.67<sup style="font-size: 20px">$</sup></h3>
+
+                    <p>Last Month</p>
+                </div>
+                <div class="icon">
+                    <i class="fa fa-dollar"></i>
+                </div>
             </div>
         </div>
 	</div>
