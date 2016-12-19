98b541f329a6220c2d6094dd2b669ae65919a4cc
Anton Vetter <vneverever@gmail.com>
2016-12-19 19:02:31 +0100

    test

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -81,6 +81,7 @@
                 <!-- /.box-body -->
             </div>
         </div>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.4.0/Chart.min.js" type="text/javascript"></script>
         <script>
             var ctx = document.getElementById('areaChart').getContext('2d');
             var myChart = new Chart(ctx, {
