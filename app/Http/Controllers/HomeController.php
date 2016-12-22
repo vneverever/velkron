553076683e5cb88cf0c8c0fc82ccdb57b8a5ffc3
Anton Vetter <vneverever@gmail.com>
2016-12-22 16:05:18 +0100

    test

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -9,7 +9,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests;
 use Illuminate\Http\Request;
-//use Khill\Lavacharts\Laravel\LavachartsFacade as Lava;
+use Khill\Lavacharts\Laravel\LavachartsFacade as Lava;
 
 /**
  * Class HomeController
