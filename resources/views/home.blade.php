7de0aeb43ffebd6fbe079ce6729fd8c0138f6c9f
Anton Vetter <vneverever@gmail.com>
2016-12-22 17:58:41 +0100

    test

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -67,7 +67,7 @@
             <div class="col-md-12">
                 <div class="box box-success">
                     <div class="box-header with-border">
-                        <h3 class="box-title">Weekly Visitors Report  -  {{{$data['sumVisitor']or 0}}}</h3>
+                        <h3 class="box-title">Weekly Visitors Report  -  {{{$data['sumVisitor'] or 0}}}</h3>
                     </div>
                     <div class="box-body" id="visitors-div">
                         {!! \Lava::render('AreaChart', 'Visitors', 'visitors-div') !!}
@@ -80,7 +80,7 @@
             <div class="col-md-12">
                 <div class="box box-success">
                     <div class="box-header with-border">
-                        <h3 class="box-title">Weekly Earnings Report  -  {{{$data['sumVisitor'] or 0}}}$</h3>
+                        <h3 class="box-title">Weekly Earnings Report  -  {{{$data['sumEarnings'] or 0}}}$</h3>
                     </div>
                     <div class="box-body" id="earnings-div">
                         {!! \Lava::render('AreaChart', 'Earnings', 'earnings-div') !!}
